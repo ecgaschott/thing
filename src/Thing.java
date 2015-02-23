4e6eaae7394575bba71572fa022909fd6dfb02a0
ecgaschott <ecgaschott@gmail.com>
2015-02-22 20:35:43 -0600

    initial commit

---
+++ src/Thing.java
@@ -0,0 +1,15 @@
+
+public class Thing {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int x = 3;
+		int y = 4;
+		
+		System.out.println("Sum: " + (x + y));
+
+	}
+
+}
